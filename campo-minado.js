67e7feafbf8938e829d2d09f831e040f4c2e6e6f
guilherme-bbsa <guilhermebarbo17@hotmail.com>
2023-02-01 14:17:26 -0300

    implement loadBomb function

--- campo-minado.js
+++ campo-minado.js
@@ -1,38 +1,34 @@
-const bombPosition = [[0, 1], [0, 2], [0, 3], [1, 1], [1, 3], [6, 4], [6, 5], [6, 6]]
-/* recebe cordenadas de uma celula e checa se a tal celula e uma bomba ou nao, caso for bomba retorna true caso n for uma bomba retorta false */
-const  isBomb = (x , y) => {
-  for(let c of bombPosition){
-    if(c[0] === x && c[1] === y){
-      return true
-    }
-  }
-  return false
-}
-/* cria uma board apartir de um parametro que define o tamanho que a board tera, e prenche cada celula da board com -1 se for uma bomba, ou 0 se n for bomba*/
+// const readlineSync = require('readline-sync');
+// const player = readlineSync.question('Digite aqui as suas cordenadas')
+// console.log(player)
+
+
 const createBoard = (boardSize) =>{
   let board = []
   for (let i = 0; i < boardSize; i++){
     let row = []
     for(let j = 0; j < boardSize; j++){
-      const value =  isBomb(i,j) ? -1: 0
-      row.push(value)
+      row.push({ isBomb: false, open: false})
     }
     board.push(row)
   }
+  loadBombs(board, boardSize)
   return board
 }
-const printBoard = (board) => {
-  if(board === 'game over'){
-    return
-  }
-  for(let row of board){
-    console.log(row.join(', '))
+
+/*Responsavel por gerar posiçoes aleatorias, defini a bomba nas posiçoes geradas aleatoriamente */
+const loadBombs = (board,boardSize) => {
+  let i = 0;
+  while(i < boardSize){
+    const x = Math.trunc(Math.random()*boardSize)
+    const y = Math.trunc(Math.random()*boardSize)
+    if(board[x][y].isBomb === false){
+      board[x][y].isBomb = true
+      i++
+    }
   }
 }
 
-/*
-retorna um array de cordenadas dos vizinhos celula dada
-*/
 const getNeighbors = (board, coordinate) => {
   const [x , y] = coordinate
   const neighbors = []
@@ -55,17 +51,13 @@ const getNeighbors = (board, coordinate) => {
   }
   return neighbors
 }
-/*
-  recebe uma board e para cada celula da board ela vai preencher :
-  * -1 se a celula tiver uma bomba
-  * se nao, numero de celulas vizinha que tenham bomba
-*/
+
 const populateBoard = (board) => {
   const newBoard = []
   for(let i = 0; i < board.length; i++) {
     const line = []
     for( let j = 0; j < board[i].length; j++) {
-      if(board[i][j] === -1){
+      if(board[i][j].isBomb){
         line.push(-1)
       }
       else{
@@ -73,7 +65,7 @@ const populateBoard = (board) => {
         let bomb = 0
         for( let n of neighbors){
           const [x , y] = n
-          if(board[x][y] === -1){
+          if(board[x][y].isBomb){
             bomb += 1
           }
         }
@@ -82,7 +74,6 @@ const populateBoard = (board) => {
     }
     newBoard.push(line)
   }
-  console.log(newBoard)
   return newBoard
 }
 
@@ -95,5 +86,15 @@ for(let i = 0; i < board.length; i++) {
     line.push(neighbors.length)
   }
   neighborsCount.push(line)
+}
+
+
+const printBoard = (board) => {
+  if(board === 'game over'){
+    return
+  }
+  for(let row of board){
+    console.log(row.join('  '))
+  }
 }
 printBoard(populateBoard(board))
